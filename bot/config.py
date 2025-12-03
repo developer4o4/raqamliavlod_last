070c6d46c54e701e15adfe54b48bcd4ddf505b03
Developer LL <new.jasjan.programmer@gmail.com>
2025-12-03 08:35:12 +0500

    fixed unix and windows paths

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,4 @@
-import os
 from aiogram import Bot, Dispatcher
-from dotenv import load_dotenv
-from pathlib import Path
-
-load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
-API_TOKEN = os.getenv("TOKEN")
 
 bot = Bot(token="7605171779:AAHT56CJwz6-JMI84imdpmkUWYJ7WhgloAI")
 dp = Dispatcher()
--- kontest/utils.py
+++ kontest/utils.py
@@ -1,11 +1,9 @@
 from django.conf import settings
 from .language_support import RunCmdGenerator
 import requests
-import json
 import time
 import shutil
 import pathlib
-
 
 DFILES = pathlib.Path(settings.BASE_DIR / 'dfiles')
 
@@ -78,8 +76,11 @@ class Code:
 
     def precheck(self):
         first_input, first_output = self.inputs[0], self.outputs[0]
+        first_output = first_output.replace('\r\n', '\n')
 
         output = self.send(first_input)
+        output = output.replace('\r\n', '\n')
+        
         if first_output == output:
             return "Correct code"
         elif output == "":
@@ -103,7 +104,6 @@ class Code:
         folder.mkdir(exist_ok=True)
         script_file = str(folder / 'script.file')
 
-        print(hasattr(RunCmdGenerator, self.language), self.language)
         if hasattr(RunCmdGenerator, self.language):
             try:
                 with open(script_file, 'w') as file:
@@ -113,16 +113,16 @@ class Code:
 
                 for index in range(len(self.inputs)):
                     file_input, file_output = self.inputs[index], self.outputs[index]
+                    file_output = file_output.replace("\r\n", "\n")
                     file_input = file_input.replace("\r", "")
                     output = func(script_file, file_input)
+                    output = output.replace("\r\n", "\n")
 
                     if output == "":
                         shutil.rmtree(str(folder))
-                        print("Error code")
                         return "Error code"
                     elif output != file_output:
                         shutil.rmtree(str(folder))
-                        print("Incorrect code")
                         return "Incorrect code"
                 shutil.rmtree(str(folder))
                 return "Correct code"
@@ -135,5 +135,6 @@ class Code:
         request_id = PaizaIO.send_request(self.code, self.language, stdin)
         while PaizaIO.check_status(request_id) != "completed":
             time.sleep(1)
+
         return PaizaIO.terminal_output(request_id).get("stdout").strip('\n').strip()
 
